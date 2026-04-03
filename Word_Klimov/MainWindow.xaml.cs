b2fb77093c72e386ad4027c4f486a21fc6b89550
STEPS0N <stepan.klimoff@mail.ru>
2026-04-03 09:34:33 +0500

    Выполненная практическая работа на оценку удовлетворительно. Климов

--- Word_Klimov/MainWindow.xaml.cs
+++ Word_Klimov/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Word_Klimov.Context;
 
 namespace Word_Klimov
 {
@@ -23,6 +25,24 @@ namespace Word_Klimov
         public MainWindow()
         {
             InitializeComponent();
+            LoadRooms();
+        }
+
+        public void LoadRooms()
+        {
+            for (int i = 1; i < 20; i++)
+                Parent.Children.Add(new Elements.Room(i));
+        }
+
+        private void Report(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Word Files (*.docx)|*.docx";
+            sfd.ShowDialog();
+            if (sfd.FileName != "")
+            {
+                OwnerContext.Report(sfd.FileName);
+            }
         }
     }
 }
