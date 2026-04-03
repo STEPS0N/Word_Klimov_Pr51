fc0dca901c4c7e25b3f41cc3a6fcc1ddc31f0164
STEPS0N <stepan.klimoff@mail.ru>
2026-04-03 09:29:31 +0500

    Разработанная модель, и контекст данных. Климов

---
+++ Word_Klimov/Context/OwnerContext.cs
@@ -0,0 +1,102 @@
+﻿using Word_Klimov.Models;
+using System.Collections.Generic;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace Word_Klimov.Context
+{
+    public class OwnerContext : Owner
+    {
+        public OwnerContext(string firstName, string lastName, string surName, int numberRoom)
+            : base(firstName, lastName, surName, numberRoom) { }
+
+        public static List<OwnerContext> AllOwners()
+        {
+            List<OwnerContext> allOwners = new List<OwnerContext>();
+
+            allOwners.Add(new OwnerContext("Елена", "Иванова", "Петровна", 1));
+            allOwners.Add(new OwnerContext("Алексей", "Смирнов", "Владимирович", 2));
+            allOwners.Add(new OwnerContext("Анна", "Кузнецова", "Сергеевна", 3));
+            allOwners.Add(new OwnerContext("Дмитрий", "Павлов", "Александрович", 3));
+            allOwners.Add(new OwnerContext("Ольга", "Михайлова", "Ивановна", 4));
+            allOwners.Add(new OwnerContext("Артем", "Козлов", "Олегович", 5));
+            allOwners.Add(new OwnerContext("Наталья", "Соколова", "Викторовна", 6));
+            allOwners.Add(new OwnerContext("Игорь", "Лебедев", "Андреевич", 6));
+            allOwners.Add(new OwnerContext("Екатерина", "Федорова", "Дмитриевна", 7));
+            allOwners.Add(new OwnerContext("Андрей", "Александрович", "Игоревич", 7));
+            allOwners.Add(new OwnerContext("Оксана", "Степанова", "Николаевна", 8));
+            allOwners.Add(new OwnerContext("Сергей", "Никитин", "Васильевич", 9));
+            allOwners.Add(new OwnerContext("Мария", "Ковалева", "Александровна", 10));
+            allOwners.Add(new OwnerContext("Павел", "Фролов", "Михайлович", 11));
+            allOwners.Add(new OwnerContext("Елена", "Белова", "Александровна", 12));
+            allOwners.Add(new OwnerContext("Илья", "Поляков", "Данилович", 13));
+            allOwners.Add(new OwnerContext("Анастасия", "Гаврилова", "Валерьевна", 14));
+            allOwners.Add(new OwnerContext("Денис", "Орлов", "Владимирович", 15));
+            allOwners.Add(new OwnerContext("Алина", "Киселева", "Сергеевна", 16));
+            allOwners.Add(new OwnerContext("Артем", "Ткаченко", "Викторович", 16));
+            allOwners.Add(new OwnerContext("Валерия", "Романова", "Павловна", 16));
+            allOwners.Add(new OwnerContext("Александр", "Максимов", "Юрьевич", 17));
+            allOwners.Add(new OwnerContext("Евгения", "Сидорова", "Игоревна", 17));
+            allOwners.Add(new OwnerContext("Никита", "Антонов", "Алексеевич", 18));
+            allOwners.Add(new OwnerContext("Юлия", "Дмитриева", "Владимировна", 19));
+
+            return allOwners;
+        }
+
+        public static void Report(string fileName)
+        {
+            Word.Application app = new Word.Application();
+            Word.Document doc = app.Documents.Add();
+            Word.Paragraph paraHeader = doc.Paragraphs.Add();
+            paraHeader.Range.Font.Size = 16;
+            paraHeader.Range.Text = "Список жильцов дома";
+            paraHeader.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            paraHeader.Range.ParagraphFormat.SpaceAfter = 0;
+            paraHeader.Range.Font.Bold = 1;
+            paraHeader.Range.InsertParagraphAfter();
+
+            Word.Paragraph paraAddress = doc.Paragraphs.Add();
+            paraAddress.Range.Font.Size = 14;
+            paraAddress.Range.Text = "по адресу: г. Пермь, ул. Луначарского, д. 24";
+            paraHeader.Range.ParagraphFormat.SpaceAfter = 20;
+            paraHeader.Range.Font.Bold = 0;
+            paraHeader.Range.InsertParagraphAfter();
+
+            Word.Paragraph paraCount = doc.Paragraphs.Add();
+            paraCount.Range.Font.Size = 14;
+            paraCount.Range.Text = $"Всего жильцов: {AllOwners().Count}";
+            paraCount.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            paraHeader.Range.ParagraphFormat.SpaceAfter = 0;
+            paraCount.Range.InsertParagraphAfter();
+
+            Word.Paragraph tableParagraph = doc.Paragraphs.Add();
+            Word.Table paymentsTable = doc.Tables.Add(tableParagraph.Range, AllOwners().Count + 1, 4);
+            paymentsTable.Borders.InsideLineStyle = paymentsTable.Borders.OutsideLineStyle = Word.WdLineStyle.wdLineStyleSingle;
+            paymentsTable.Range.Cells.VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+
+            Cell("№", paymentsTable.Cell(1, 1).Range);
+            Cell("Фамилия", paymentsTable.Cell(1, 2).Range);
+            Cell("Имя", paymentsTable.Cell(1, 3).Range);
+            Cell("Отчество", paymentsTable.Cell(1, 4).Range);
+
+            for (int i = 0; i < AllOwners().Count; i++)
+            {
+                OwnerContext owner = AllOwners()[i];
+                Cell((i + 1).ToString(), paymentsTable.Cell(1 + 1 + i, 1).Range);
+                Cell(owner.LastName, paymentsTable.Cell(1 + 1 + i, 2).Range, Word.WdParagraphAlignment.wdAlignParagraphLeft);
+                Cell(owner.FirstName, paymentsTable.Cell(1 + 1 + i, 3).Range, Word.WdParagraphAlignment.wdAlignParagraphLeft);
+                Cell(owner.SurName, paymentsTable.Cell(1 + 1 + i, 4).Range, Word.WdParagraphAlignment.wdAlignParagraphLeft);
+            }
+
+            doc.SaveAs2(fileName);
+            doc.Close();
+            app.Quit();
+        }
+
+        private static void Cell(string Text, Word.Range Cell,
+            Word.WdParagraphAlignment Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter)
+        {
+            Cell.Text = Text;
+            Cell.ParagraphFormat.Alignment = Alignment;
+        }
+    }
+}
